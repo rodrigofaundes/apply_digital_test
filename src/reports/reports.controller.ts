52ca31efa24664ebfbfc9b74ec2c055f73cf931d
Rodrigo Faundes <rodrigofaundez93@gmail.com>
2025-01-21 01:06:12 -0300

    reports module with filter by deleted and not deleted with range of dates

---
+++ src/reports/reports.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { GetReportByFiltersDto } from './dto/get-report-filter.reports.dto';
+
+@Controller('reports')
+export class ReportsController {
+
+  constructor(
+    private readonly reportsService: ReportsService
+  ){}
+
+  @Get('deleted-percentage')
+  getDeletedPercentage() {
+      return this.reportsService.getReportDeletedProducts();
+  }
+
+  @Get('not-deleted-percentage')
+  getNotDeletedPercentage( @Query() params?: GetReportByFiltersDto ) {
+    console.log("🚀 ~ file: reports.controller.ts:16 ~ ReportsController ~ params:", params)
+    return this.reportsService.getReportByFilters(params);
+  }
+}
