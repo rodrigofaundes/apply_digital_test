0a3a7895838c133dc3f3ed7cad20089616b47526
Rodrigo Faundes <rodrigofaundez93@gmail.com>
2025-01-20 22:38:52 -0300

    Filter and paginate products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, ParseUUIDPipe } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
@@ -11,19 +11,50 @@ export class ProductsService {
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
 ) {}
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
   findAll() {
     return "this.productRepository.find({where: {deleted: false}})"
   }
 
-  findOne(id: string) {
-    return "this.productRepository.find({where: {id, deleted: false}})"
+  async findOne(id: string) {
+    const product =  await this.productRepository.findOne({
+      where: { id, deleted: false },
+    })
+
+    if ( !product ) throw new NotFoundException(`Product with id '${ id }' not found`);
+      
+    return product
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    await this.findOne(id)
+    return this.productRepository.update(id, { deleted: true });
+  }
+
+  async paginate(where: any, skip: number, take: number, filters: { minPriceNum?: number, maxPriceNum?: number }){
+    const query = this.productRepository.createQueryBuilder('product');
+
+    query.where('product.deleted = :deleted', { deleted: false });
+
+    if (where.name) {
+        query.andWhere('product.name LIKE :name', { name: `%${where.name}%` });
+    }
+
+    if (where.category) {
+        query.andWhere('product.category = :category', { category: where.category });
+    }
+
+    if (filters.minPriceNum) {
+        query.andWhere('product.price >= :minPriceNum', { minPriceNum: filters.minPriceNum });
+    }
+
+    if (filters.maxPriceNum) {
+        query.andWhere('product.price <= :maxPriceNum', { maxPriceNum: filters.maxPriceNum });
+    }
+
+    query.skip(skip).take(take);
+
+    const [items, total] = await query.getManyAndCount();
+    return [items, total];
   }
 }
