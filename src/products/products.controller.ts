78f8ad9f74b656f1385da22fca58f9d3ec231f15
Rodrigo Faundes <rodrigofaundez93@gmail.com>
2025-01-21 01:05:31 -0300

    Delete deprecated endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,16 +6,6 @@ import { FindProductDto } from './dto/find-product.dto';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Get()
-  findAll() {
-    return this.productsService.findAll();
-  }
-
-  @Get('find/:id')
-  findOne(@Param() params: FindProductDto) {
-    return this.productsService.findOne(params.id);
-  }
-
   @Delete(':id')
   remove(@Param() params: FindProductDto) {
     return this.productsService.remove(params.id);
@@ -30,24 +20,24 @@ export class ProductsController {
     @Query('maxPrice') maxPrice?: string,
   ) {
 
-	const minPriceNum = minPrice ? parseFloat(minPrice) : undefined;
-	const maxPriceNum = maxPrice ? parseFloat(maxPrice) : undefined;
-	const take = 5;
-	const skip = (page - 1) * take;
-	let where: any = { deleted: false };
+    const minPriceNum = minPrice ? parseFloat(minPrice) : undefined;
+    const maxPriceNum = maxPrice ? parseFloat(maxPrice) : undefined;
+    const take = 5;
+    const skip = (page - 1) * take;
+    let where: any = { deleted: false };
 
-	if (name) where.name = name;
-	if (category) where.category = category;
+    if (name) where.name = name;
+    if (category) where.category = category;
 
-	const [items, total] = await this.productsService.paginate(where, skip, take, {
-	  minPriceNum,
-	  maxPriceNum,
-	});
+    const [items, total] = await this.productsService.paginate(where, skip, take, {
+      minPriceNum,
+      maxPriceNum,
+    });
 
-	return {
-	  page,
-	  total,
-	  items
-	}
+    return {
+      page,
+      total,
+      items
+    }
   }
 }
