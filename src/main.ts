59cc37f96971c284a2a566475bb7490f95be2fcc
Rodrigo Faundes <rodrigofaundez93@gmail.com>
2025-01-22 17:17:19 -0300

    Documentation page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Apply Digital Test')
+    .setDescription('API for sync products, delete and generate reports')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
