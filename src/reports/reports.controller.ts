955e9bdebeb020764708c41bf182fd637aefbecd
Rodrigo Faundes <rodrigofaundez93@gmail.com>
2025-01-22 19:38:49 -0300

    Add auth to report controller and modify .spec for that module

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { GetReportByFiltersDto } from './dto/get-report-filter.reports.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('reports') export class ReportsController {
 
     constructor(private readonly reportsService: ReportsService) {}
-
-    @Get('deleted-percentage') getDeletedPercentage() {
+    
+    @Get('deleted-percentage') 
+    getDeletedPercentage() {
         return this.reportsService.getReportDeletedProducts();
     }
 
-    @Get('not-deleted-percentage') getNotDeletedPercentage(@Query() params?: GetReportByFiltersDto) {
+    @Get('not-deleted-percentage') 
+    getNotDeletedPercentage(@Query() params?: GetReportByFiltersDto) {
         return this.reportsService.getReportByFilters(params);
     }
 }
